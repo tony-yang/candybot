80d5f7c2e3747fc86b7d6cff6e4167f52201861d
Tony Yang <tianyuyang658@yahoo.com>
2023-10-15 21:41:10 -0700

    Add v2 candybot code, based on a new hardware setup that is simpler and more robust, without voice recognition

---
+++ candybotv2.py
@@ -0,0 +1,83 @@
+import RPi.GPIO as GPIO
+import time
+
+enable = 12 # (GPIO 18)
+in1 = 37 # Motor input 1 (GPIO 26)
+in2 = 36 # Motor input 2 (GPIO 16)
+trigger = 31 # Ultrasonic trigger (GPIO 6)
+echo = 29 # Ultrasonic echo (GPIO 5)
+
+def init_hardware():
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(enable, GPIO.OUT)
+  GPIO.setup(in1, GPIO.OUT)
+  GPIO.setup(in2, GPIO.OUT)
+  GPIO.setup(trigger, GPIO.OUT)
+  GPIO.setup(echo, GPIO.IN)
+
+def enable_motor():
+  GPIO.output(enable, GPIO.HIGH)
+
+def disable_motor():
+  GPIO.output(enable, GPIO.LOW)
+
+def forward():
+  enable_motor()
+  GPIO.output(in1, GPIO.HIGH)
+  GPIO.output(in2, GPIO.LOW)
+  time.sleep(3)
+  disable_motor()
+
+def measured_distance():
+  GPIO.output(trigger, False)
+  print(f'Waiting for sensor to settle')
+  time.sleep(2)
+
+  GPIO.output(trigger, GPIO.HIGH)
+  time.sleep(0.00001)
+  GPIO.output(trigger, GPIO.LOW)
+
+  start_time = time.time()
+  stop_time = time.time()
+
+  while GPIO.input(echo) == 0:
+    start_time = time.time()
+
+  while GPIO.input(echo) == 1:
+    stop_time = time.time()
+
+  time_delta = stop_time - start_time
+  return round(time_delta * 34300 / 2, 2)
+
+def person_detected():
+  dist = measured_distance()
+  # Distance in cm
+  if dist > 20 and dist < 160:
+    print(f'Person detected at {dist} cm')
+    return True
+  return False
+
+def send_candy():
+  print(f'Send Candy')
+  forward()
+
+def main():
+  init_hardware()
+  while True:
+    time.sleep(8)
+    print(f'\n\n####################################')
+    while not person_detected():
+      time.sleep(2)
+
+    try:
+      send_candy()
+    except Exception as e:
+      print(f'No more fall back, error: {e}')
+
+if __name__ == '__main__':
+  try:
+    main()
+  except KeyboardInterrupt:
+    print(f'Stopped by userr')
+  finally:
+    GPIO.cleanup()
